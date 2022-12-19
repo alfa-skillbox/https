0214f4ef7b418b9e63900be55d83ccc67c10e624
Aleksandr A. Ivanov <aivanov10@alfabank.ru>
2022-12-20 02:11:50 +0300

    finished http pure code and scripts 2

--- gateway/src/main/java/ru/alfabank/skillbox/examples/https/server/controller/ClientController.java
+++ gateway/src/main/java/ru/alfabank/skillbox/examples/https/server/controller/ClientController.java
@@ -1,6 +1,7 @@
 package ru.alfabank.skillbox.examples.https.server.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.alfabank.skillbox.examples.https.server.restclient.dto.ResourceServerResponse;
+import ru.alfabank.skillbox.examples.https.server.dto.Response;
 import ru.alfabank.skillbox.examples.https.server.service.RestService;
 
 import javax.validation.constraints.NotEmpty;
@@ -28,12 +29,20 @@ public class ClientController {
     private final RestService restService;
 
     @PostMapping("/save")
-    public ResponseEntity<ResourceServerResponse> validateAndSave(@RequestBody @NotEmpty String rawJson) {
-        return ResponseEntity.ok(restService.invokeSave(rawJson));
+    public ResponseEntity<Response> validateAndSave(@RequestBody @NotEmpty String rawJson) {
+        var restResponse = restService.invokeSave(rawJson);
+        return ResponseEntity.ok(Response.builder()
+                .status(HttpStatus.OK.name())
+                .body(restResponse)
+                .build());
     }
 
     @GetMapping("/find")
-    public ResponseEntity<String> findById(@RequestParam("id") String id) {
-        return ResponseEntity.ok(restService.invokeFind(id));
+    public ResponseEntity<Response> findById(@RequestParam("id") String id) {
+        var restResponse = restService.invokeFind(id);
+        return ResponseEntity.ok(Response.builder()
+                .status(HttpStatus.OK.name())
+                .body(restResponse)
+                .build());
     }
 }
