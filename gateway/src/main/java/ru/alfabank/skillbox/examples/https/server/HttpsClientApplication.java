74d1c8609ba920befcb2aef14f509810045087b9
Aleksandr A. Ivanov <aivanov10@alfabank.ru>
2022-12-20 01:52:35 +0300

    removed feign in client

--- gateway/src/main/java/ru/alfabank/skillbox/examples/https/server/HttpsClientApplication.java
+++ gateway/src/main/java/ru/alfabank/skillbox/examples/https/server/HttpsClientApplication.java
@@ -2,9 +2,8 @@ package ru.alfabank.skillbox.examples.https.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+
 @SpringBootApplication
 public class HttpsClientApplication {
 
